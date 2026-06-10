7e1af99a2cf26bc42bd64c2e18c18a17ddded08b
yorimek <yorimek@f4r2s15.paris.42.school>
2026-06-10 10:48:57 +0200

    update: ex03 fixed (initialize HumanB weapon)

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,14 +6,14 @@
 /*   By: yorimek <yorimek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 15:20:29 by yorimek           #+#    #+#             */
-/*   Updated: 2026/06/05 16:21:08 by yorimek          ###   ########.fr       */
+/*   Updated: 2026/06/10 10:48:28 by yorimek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/HumanB.hpp"
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name)
+HumanB::HumanB(std::string name): _weapon(NULL), _name(name)
 {
 
 }
@@ -26,7 +26,10 @@ HumanB::~HumanB()
 
 void	HumanB::attack() const
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (_weapon != NULL)
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	else
+		std::cout << this->_name << " has no weapon." << std::endl;
 }
 
 void HumanB::setWeapon(Weapon &weapon)
